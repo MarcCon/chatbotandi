d9c91cfbf9cafbc4e483bc16eb79d5cbea98ef15
Marc-Constantin Enke <marc.enke@aol.de>
2024-08-05 21:59:09 +0200

    first commit

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "asst_CJdnwXvtUESBVqIc7MhiHvcq"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
